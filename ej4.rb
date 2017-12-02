c32da3429bf0543f32547472fc63bb3acbb38cc4
MatiasCarrara <ezequielcarara777@gmail.com>
2017-12-02 00:29:21 -0300

    nada

--- ej4.rb
+++ ej4.rb
@@ -1,6 +1,24 @@
 # ej4.rb
 module Exercise4
-  def hola
-
+  def self.hola(gg, lol)
+    @juan = gg
+    @pedro = lol
+    @pedro -= 1
+    @yo = 0
+    if @pedro > -1
+      if @pedro > @juan.length
+        puts @juan
+      else
+        while @juan[@yo] <= @juan[0 + @pedro]
+          puts @juan[@yo]
+          @yo += 1
+        end
+      end
+    else
+      puts 'NADA'
+    end
   end
 end
+string1 = 'a', 'b', 'c', 'd', 'e'
+string2 = 0
+Exercise4.hola(string1, string2)
---
+++ spec/ej4_spec.rb
@@ -0,0 +1,12 @@
+# ej4_spec.rb
+require '/home/mooveit/MyGit/Induccion_anima/ej4.rb'
+
+describe Exercise4 do
+  describe '.hola' do
+    context '' do
+      it '' do
+        expect(Exercise4.hola).to be_between(10.00, 100.00)
+      end
+    end
+  end
+end
