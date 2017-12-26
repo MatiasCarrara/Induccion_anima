b874e9535ef37a02abd83508db5e7c6c5521ebe3
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2017-12-26 12:05:32 -0300

    nada

--- ej4.rb
+++ ej4.rb
@@ -1,19 +1,18 @@
 module Exercise4
-  def self.take(alphs, beta)
-    @alpha = alphs
-    @beta = beta -= 1
+  def self.take(alpha, beta)
+    beta -= 1
     @var = 0
-    if @beta > -1
-      if @beta > @alpha.length
-        puts @alpha
+    if beta > -1
+      if beta > alpha.length
+         alpha
       else
-        while @var <= @beta
-          puts @alpha[@var]
+        while @var <= beta
+           alpha[@var]
           @var += 1
         end
       end
     else
-      puts 'nothing'
+       'nothing'
     end
   end
 end
--- spec/ej4_spec.rb
+++ spec/ej4_spec.rb
@@ -1,4 +1,4 @@
-require_relative '/Users/matiascarrara/Git/Induccion_anima/ej4.rb'
+require_relative '../ej4.rb'
 
 describe Exercise4 do
   describe '.take' do
@@ -6,28 +6,31 @@ describe Exercise4 do
       it 'returns the contents of the first 3 array positions' do
         string1 = 'a', 'b', 'c', 'd', 'e'
         string2 = 3
-        expect(Exercise4.take(string1, string2)).to eql(puts('a', 'b', 'c'))
+        string3 = 'a', 'b', 'c'
+        expect(Exercise4.take(string1, string2)).to eql(string3)
       end
     end
     context 'we passed a string with the character 3' do
       it 'shows the contents of position 1 and 2 of the matrix, the thrid is empty, it shows nothing' do
         string1 = 'a', 'b', '', 'd', 'e'
         string2 = 3
-        expect(Exercise4.take(string1, string2)).to eql(puts('a','b',''))
+        string3 = 'a','b',''
+        expect(Exercise4.take(string1, string2)).to eql(string3)
       end
     end
     context 'we passed a string with the character 0' do
       it 'returns "nothing", because position 0 does not exist' do
         string1 = 'a', 'b', 'c', 'd', 'e'
         string2 = 0
-        expect(Exercise4.take(string1, string2)).to eql(puts('nothing'))
+        expect(Exercise4.take(string1, string2)).to eql('nothing')
       end
     end
     context 'we passed a string with the character 27' do
       it 'shows the entire array' do
         string1 = 'a', 'b', 'c', 'd', 'e'
         string2 = 27
-        expect(Exercise4.take(string1, string2)).to eql(puts('a', 'b', 'c', 'd', 'e'))
+        string3 = 'a', 'b', 'c', 'd', 'e'
+        expect(Exercise4.take(string1, string2)).to eql(string3)
       end
     end
   end
