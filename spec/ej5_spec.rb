d830eae74f1a3e8f3598e3f88933aad513d1765b
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2017-12-26 13:09:03 -0300

    nada

--- spec/ej5_spec.rb
+++ spec/ej5_spec.rb
@@ -1,30 +1,29 @@
-require_relative '/Users/matiascarrara/Git/Induccion_anima/ej5.rb'
-
+require_relative '../ej5.rb'
 describe Exercise5 do
   describe '.case' do
     context 'a word in capital' do
       it 'the same word in lowercase' do
-        expect(Exercise5.case('MATIAS')).to eql(puts('matias'))
+        expect(Exercise5.case('MATIAS')).to eql('matias')
       end
     end
     context 'a word in lowercase' do
       it 'the same word in capital' do
-        expect(Exercise5.case('matias')).to eql(puts('MATIAS'))
+        expect(Exercise5.case('matias')).to eql('MATIAS')
       end
     end
     context 'pass "mAtIAs"' do
       it 'return "MaTiaS"' do
-        expect(Exercise5.case('mAtIAs')).to eql(puts('MaTiaS'))
+        expect(Exercise5.case('mAtIAs')).to eql('MaTiaS')
       end
     end
     context 'pass numbers' do
       it 'return the same numbers' do
-        expect(Exercise5.case('123')).to eql(puts('123'))
+        expect(Exercise5.case('123')).to eql('123')
       end
     end
     context 'pass nothing' do
       it 'return nothing' do
-        expect(Exercise5.case('')).to eql(puts(''))
+        expect(Exercise5.case('')).to eql('')
       end
     end
   end
