cc118727eb5d176cb51ad09c065a743da35daa50
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2017-12-27 11:18:32 -0300

    nada

--- spec/ej1_spec.rb
+++ spec/ej1_spec.rb
@@ -2,25 +2,21 @@ require_relative '../ej1.rb'
 
 describe Exercise1 do
   describe '.biggest' do
-    context 'Single word' do
-      it 'that same word' do
-        expect(Exercise1.biggest(%W[hola])).to eql(("hola"))
+    context do
+      subject { Exercise1.biggest(%w[hola]) }
+        it { is_expected.to eq(('hola')) }
       end
+      context do
+      subject {Exercise1.biggest(%w[Matias Valentina])}
+        it {is_expected.to eq(('Valentina'))}
+      end
+      context do
+      subject {Exercise1.biggest(%w[mati vale ])}
+        it {is_expected.to eq(('mati'))}
     end
-    context 'more than one word' do
-      it 'it is the longest word' do
-        expect(Exercise1.biggest(%w[Matias Valentina])).to eql(('Valentina'))
-      end
-    end
-    context 'two equal words' do
-      it 'the first written word' do
-        expect(Exercise1.biggest(%w[Mati Vale])).to eql(('Mati'))
-      end
-    end
-    context 'not any word' do
-      it 'does not return anything' do
-        expect(Exercise1.biggest(%W[])).to eql(nil)
-      end
+    context do
+      subject {Exercise1.biggest(%w[])}
+        it {is_expected.to eq((nil))}
     end
   end
 end
