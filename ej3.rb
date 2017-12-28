775085ca147f17f9cb24250df4d8499c9820531c
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2017-12-28 10:41:59 -0300

    Ejercicio3

--- ej3.rb
+++ ej3.rb
@@ -2,12 +2,8 @@
 module Exercise3
   def self.contains?(array, to_verify)
     array.each do |here|
-      @var = to_verify if to_verify == here
-    end
-    if @var == to_verify
-      true
-    else
-      false
-    end
+        return true if here == to_verify
+      end
+        return false
   end
 end
