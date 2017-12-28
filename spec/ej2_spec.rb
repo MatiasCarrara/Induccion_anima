592661045369f81aba0ecbb101599478ef8d9bc4
MatiasCarrara <MatiasCarrara@users.noreply.github.com>
2017-12-28 10:14:36 -0300

    Ejercicio 2 (#2)
    * ej2
    
    * ej2
    
    * Ejercicio2
    
    * Ejercicio2

---
+++ spec/ej2_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../ej2.rb'
+describe Exercise2 do
+  describe '.random' do
+      it 'returns a random number between 10.00 and 100.00' do
+        expect(Exercise2.random).to be_between(10.00, 100.00)
+      end
+  end
+end
