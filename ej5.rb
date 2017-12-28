d3b53e8898d5b662e2c06a307f752b5e92c8d443
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2017-12-28 09:09:33 -0300

    Ejercicio5

--- ej5.rb
+++ ej5.rb
@@ -1,16 +1,16 @@
 module Exercise5
   def self.upper_lower(text)
-    @var = 0
-    @here = 0
+    alpha = 0
+    beta = 0
     content = ''
-    while @var < text.length
-      if text[0 + @here] == text[0 + @here].upcase
-          content += text[0 + @here].downcase
+    while alpha < text.length
+      if text[0 + beta] == text[0 + beta].upcase
+          content += text[0 + beta].downcase
       else
-         content += text[0 + @here].upcase
+         content += text[0 + beta].upcase
       end
-      @var += 1
-      @here += 1
+      alpha += 1
+      beta += 1
     end
     return content
   end
