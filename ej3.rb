1998a183ac28016cf0167acc9507f2f18f5959e3
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2017-12-28 10:43:10 -0300

    Ejercicio3

--- ej3.rb
+++ ej3.rb
@@ -1,8 +1,8 @@
 # ej3.rb
 module Exercise3
   def self.contains?(array, to_verify)
-    array.each do |here|
-        return true if here == to_verify
+    array.each do |var|
+        return true if var == to_verify
       end
         return false
   end
