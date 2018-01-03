4eb187d0f8fa913a687cf3d588d3dc1580048670
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-03 09:03:02 -0300

    nada

--- ej1.rb
+++ ej1.rb
@@ -1,10 +1,11 @@
+# ej1.rb
 module Exercise1
   def self.biggest(data)
     longest = data.first
     data.each do |var|
       if var.length > longest.length
-       longest = var
-     end
+        longest = var
+      end
     end
     longest
   end
--- spec/ej1_spec.rb
+++ spec/ej1_spec.rb
@@ -2,21 +2,21 @@ require_relative '../ej1.rb'
 
 describe Exercise1 do
   describe '.biggest' do
-    context do
+    context 'we passed a single word' do
       subject { Exercise1.biggest(%w[hola]) }
-        it { is_expected.to eq(('hola')) }
-      end
-      context do
-      subject {Exercise1.biggest(%w[Matias Valentina])}
-        it {is_expected.to eq(('Valentina'))}
-      end
-      context do
-      subject {Exercise1.biggest(%w[mati vale ])}
-        it {is_expected.to eq(('mati'))}
+        it { is_expected.to match('hola') }
     end
-    context do
-      subject {Exercise1.biggest(%w[])}
-        it {is_expected.to eq((nil))}
+    context 'we passed two words of different length' do
+      subject { Exercise1.biggest(%w[Matias Valentina]) }
+        it { is_expected.to match('Valentina') }
+    end
+    context 'we passed two words of the same length' do
+      subject { Exercise1.biggest(%w[mati vale]) }
+        it { is_expected.to match('mati') }
+    end
+    context 'we do not pass any word' do
+      subject { Exercise1.biggest(%w[]) }
+        it { is_expected.to match (nil) }
     end
   end
 end
