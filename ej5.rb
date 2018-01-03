79fca3239366d3528d1213070ed0ab511acecc98
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-03 10:47:17 -0300

    Ejercicio5

--- ej5.rb
+++ ej5.rb
@@ -1,15 +1,17 @@
+# ej5.rb
 module Exercise5
   def self.upper_lower(text)
     var = 0
     content = ''
     while var < text.length
-      if text[0 + var] == text[0 + var].upcase
-          content += text[0 + var].downcase
-      else
-         content += text[0 + var].upcase
-      end
+      content +=
+        if text[0 + var] == text[0 + var].upcase
+          text[0 + var].downcase
+        else
+          text[0 + var].upcase
+        end
       var += 1
     end
-     content
+    content
   end
 end
--- spec/ej5_spec.rb
+++ spec/ej5_spec.rb
@@ -2,24 +2,24 @@ require_relative '../ej5.rb'
 describe Exercise5 do
   describe '.upper_lower' do
     context 'a word in capital' do
-      subject{ Exercise5.upper_lower('MATIAS')}
-      it { is_expected.to eq('matias')}
+      subject { Exercise5.upper_lower('MATIAS') }
+      it { is_expected.to eq('matias') }
     end
     context 'a word in lowercase' do
-      subject{ Exercise5.upper_lower('matias')}
-      it { is_expected.to eq('MATIAS')}
+      subject { Exercise5.upper_lower('matias') }
+      it { is_expected.to eq('MATIAS') }
     end
     context 'pass "mAtIAs"' do
-      subject{ Exercise5.upper_lower('mAtIAs')}
-      it { is_expected.to eq('MaTiaS')}
+      subject { Exercise5.upper_lower('mAtIAs') }
+      it { is_expected.to eq('MaTiaS') }
     end
     context 'pass numbers' do
-        subject{ Exercise5.upper_lower('123')}
-        it { is_expected.to eq('123')}
+      subject { Exercise5.upper_lower('123') }
+      it { is_expected.to eq('123') }
     end
     context 'pass nothing' do
-      subject{ Exercise5.upper_lower('')}
-      it { is_expected.to eq('')}
+      subject { Exercise5.upper_lower('') }
+      it { is_expected.to eq('') }
     end
   end
 end
