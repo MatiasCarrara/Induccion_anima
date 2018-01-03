013f48db8818e7b880515d1b6454dbb2bacc130d
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-03 11:01:39 -0300

    Ejercicio4

--- ej4.rb
+++ ej4.rb
@@ -1,12 +1,11 @@
 # ej3.rb
 module Exercise4
   def self.select(array, select_num)
-    if select_num > -1
-      if select_num >= array.length
-        array
-      else
-        array[0, select_num]
-      end
+    return unless select_num > -1
+    if select_num >= array.length
+      array
+    else
+      array[0, select_num]
     end
   end
 end
--- spec/ej4_spec.rb
+++ spec/ej4_spec.rb
@@ -7,24 +7,24 @@ describe Exercise4 do
       let(:string2) { 3 }
       let(:string3) { %w[a b c] }
       subject { Exercise4.select(string, string2) }
-        it { is_expected.to eq(string3) }
+      it { is_expected.to eq(string3) }
     end
     context 'we passed a string with the character 4' do
       string1 = 'a', 'b', 'c', '', 'e'
       let(:string2) { 4 }
       string3 = 'a', 'b', 'c', ''
       subject { Exercise4.select(string1, string2) }
-        it { is_expected.to eq(string3) }
+      it { is_expected.to eq(string3) }
     end
     context 'we passed a string with the character 27' do
       let(:string2) { 27 }
       subject { Exercise4.select(string, string2) }
-        it { is_expected.to eq(string) }
+      it { is_expected.to eq(string) }
     end
     context 'we passed a string with the character 5' do
       let(:string2) { 5 }
       subject { Exercise4.select(string, string2) }
-        it { is_expected.to eq(string) }
+      it { is_expected.to eq(string) }
     end
   end
 end
