9f53b8c00e1bb2ff7974c7c543fa6b1489d27590
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-04 11:43:57 -0300

    Ejercicio3

--- spec/ej3_spec.rb
+++ spec/ej3_spec.rb
@@ -4,24 +4,24 @@ describe Exercise3 do
   describe '.contains?' do
     let(:string) { %w[a b c] }
     context 'I pass a letter that is in the array' do
-      let(:string2) { 'a' }
-      subject { Exercise3.contains?(string, string2) }
+      let(:letter) { 'a' }
+      subject { Exercise3.contains?(string, letter) }
       it { is_expected.to eq(true) }
     end
     context 'I pass a letter that is not in the array' do
-      let(:string2) { 'r' }
-      subject { Exercise3.contains?(string, string2) }
+      let(:letter) { 'r' }
+      subject { Exercise3.contains?(string, letter) }
       it { is_expected.to eq(false) }
     end
     context 'I pass an empty space, in a full array' do
-      let(:string2) { '' }
-      subject { Exercise3.contains?(string, string2) }
+      let(:letter) { '' }
+      subject { Exercise3.contains?(string, letter) }
       it { is_expected.to eq(false) }
     end
     context 'I pass an empty space, in a array with an empty space' do
-      let(:string1) { ['a', 'b', ''] }
-      let(:string2) { '' }
-      subject { Exercise3.contains?(string1, string2) }
+      let(:array) { ['a', 'b', ''] }
+      let(:letter) { '' }
+      subject { Exercise3.contains?(array, letter) }
       it { is_expected.to eq(true) }
     end
   end
