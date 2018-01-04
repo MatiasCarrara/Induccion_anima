d8a73bdae813a5cde1bf28eb59ab925ea3bcdb80
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-04 11:41:29 -0300

    Ejercicio4

--- spec/ej4_spec.rb
+++ spec/ej4_spec.rb
@@ -4,26 +4,26 @@ describe Exercise4 do
   describe '.select' do
     let(:string) { %w[a b c d e] }
     context 'we passed a string with the character 3' do
-      let(:string2) { 3 }
-      let(:string3) { %w[a b c] }
-      subject { Exercise4.select(string, string2) }
-      it { is_expected.to eq(string3) }
+      let(:number) { 3 }
+      let(:answer) { %w[a b c] }
+      subject { Exercise4.select(string, number) }
+      it { is_expected.to eq(answer) }
     end
     context 'we passed a string with the character 4' do
       let(:string1) { ['a', 'b', 'c', '', 'e'] }
-      let(:string2) { 4 }
-      let(:string3) { ['a', 'b', 'c', ''] }
-      subject { Exercise4.select(string1, string2) }
-      it { is_expected.to eq(string3) }
+      let(:number) { 4 }
+      let(:answer) { ['a', 'b', 'c', ''] }
+      subject { Exercise4.select(string1, number) }
+      it { is_expected.to eq(answer) }
     end
     context 'we passed a string with the character 27' do
-      let(:string2) { 27 }
-      subject { Exercise4.select(string, string2) }
+      let(:number) { 27 }
+      subject { Exercise4.select(string, number) }
       it { is_expected.to eq(string) }
     end
     context 'we passed a string with the character 5' do
-      let(:string2) { 5 }
-      subject { Exercise4.select(string, string2) }
+      let(:number) { 5 }
+      subject { Exercise4.select(string, number) }
       it { is_expected.to eq(string) }
     end
   end
